782946106d1d8ae2eb62798eb3631a79e0280a96
ayowilfred95 <38457345+ayowilfred95@users.noreply.github.com>
2022-06-02 18:29:21 +0100

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://ayowilfred:metropolitan95@cluster0.uhwgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+mongoose.connect("mongodb_url",{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology:true
